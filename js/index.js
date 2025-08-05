3c021332fd77742ff12db95fb1de6f4425f60508
Yulianna Burkovska <juliburkovska@gmail.com>
2025-08-05 22:59:40 +0300

    fix

--- js/index.js
+++ js/index.js
@@ -3,5 +3,5 @@ const team = document.getElementById("team");
 team.style.color="green";
 
 const slogan = document.getElementById("slogan")
-slogan.style.color ="gray";
+slogan.style.color ="graygit ";
 
